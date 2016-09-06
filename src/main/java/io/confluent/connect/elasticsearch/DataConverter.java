bd187c44f76e336a7a5257c52d5463c9d6d6c20b
rmoff <rmoff@asgard-3.local>
2016-09-06 09:36:28 +0100

    Add some trace logging to troubleshoot ignoreKey issue (https://github.com/confluentinc/kafka-connect-elasticsearch/issues/23)

--- src/main/java/io/confluent/connect/elasticsearch/DataConverter.java
+++ src/main/java/io/confluent/connect/elasticsearch/DataConverter.java
@@ -118,15 +118,18 @@ public class DataConverter {
     boolean topicIgnoreSchema;
 
     if (topicConfigs.containsKey(topic)) {
+      log.trace("Topic found in topicConfigs");
       TopicConfig topicConfig = topicConfigs.get(topic);
       index = topicConfig.getIndex();
       topicIgnoreKey = topicConfig.ignoreKey();
       topicIgnoreSchema = topicConfig.ignoreSchema();
     } else {
+      log.trace("Topic not found in topicConfigs");
       index = topic;
       topicIgnoreKey = ignoreKey;
       topicIgnoreSchema = ignoreSchema;
     }
+    log.trace("Config values: index {} / ignoreKey {} / ignoreSchema {}", index,topicIgnoreKey, topicIgnoreSchema);
 
     if (topicIgnoreKey) {
       id = topic + "+" + String.valueOf(partition) + "+" + String.valueOf(offset);
